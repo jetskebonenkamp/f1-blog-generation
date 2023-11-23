d43757c8234e742f3a6fc5d83f800c6dd6e14fa6
jetskebonenkamp <j.bonenkamp@student.utwente.nl>
2023-11-23 14:37:32 +0100

    comments added

--- LinguisticFeatureExtraction/LFE.py
+++ LinguisticFeatureExtraction/LFE.py
@@ -6,43 +6,43 @@ import json
 
 
 def loadPipe(folder):
-    with open(folder + './NamedEntityRecognition/ner_training_data2.ob', 'rb') as f:
-        train_data = pickle.load(f)
+    # create nlp pipeline and further train a NER model for our scope
+    model_path = folder + './NamedEntityRecognition/ner_training_data.ob'
+    with open(model_path, 'rb') as f: train_data = pickle.load(f)
     pipe = NLPPipeline('en_core_web_sm')
     pipe.trainNerModel(train_data)
     return pipe
 
 
-def extractFeatures():
+def extractFeatures(blogs, pipe):
+    # boundaries set to exclude pre- and post-race posts
+    blogs, blog_objects = blogs[15:len(blogs)-15], {}
+    for blog in blogs:
+        fe = FeatureExtractor(blog, pipe.nlp)
+        data_objs = fe.getDataObjects()
+        if data_objs:
+            blog_objects[blog] = []
+            for obj in data_objs: blog_objects[blog].append(obj)
+    return blog_objects
+
+
+def extractAllFeatures():
+    # retrieve blog posts and extract data objects
     folder = '../../Data Extraction/BlogData/'
-    all_blogs = loadBlogData(folder + 'JSONData_Autosport3', all_files=True)
-    pipe = loadPipe(folder)
-    all_blog_objects = {}
+    all_blogs = loadBlogData(folder + 'DataSet', all_files=True)
+    pipe, all_blog_objects = loadPipe(folder), {}
     for key in list(all_blogs.keys()):
-        blogs = all_blogs[key]
-        # since the first and last sequence of blog posts
-        # contain pre- and post-race content, the first
-        # and last 15 items are excluded
-        blogs = blogs[15:len(blogs)-15]
-        blog_objects = {}
-        for blog in blogs:
-            fe = FeatureExtractor(blog, pipe.nlp)
-            data_objs = fe.getDataObjects()
-            if data_objs:
-                blog_objects[blog] = []
-                for obj in data_objs:
-                    blog_objects[blog].append(obj)
-        all_blog_objects[key] = blog_objects
+        all_blog_objects[key] = extractFeatures(all_blogs[key], pipe)
     return all_blog_objects
 
 
-def establishDataset():
+def saveIoPairs():
+    # save extracted data objects
     all_blog_objects = extractFeatures()
     for key in list(all_blog_objects.keys()):
         blog_objects = all_blog_objects[key]
         fn = 'IOPairs/IO_' + key + '.json'
-        with open(fn, 'w') as f:
-            json.dump(blog_objects, f)
+        with open(fn, 'w') as f: json.dump(blog_objects, f)
 
 
-establishDataset()
+saveIoPairs()
